8606e08f9883ac612272e2d221d48f5b939a6cc7
Gregory R. Lee <grlee77@gmail.com>
2016-01-22 02:06:24 -0500

    refactoring of ctypes/cython code

--- _extensions/setup.py
+++ _extensions/setup.py
@@ -28,7 +28,7 @@ def configuration(parent_package='', top_path=None):
 
     config.add_extension(
         "_dtft",
-        sources=['_dtft.pyx'],
+        sources=['_dtft.c'],
         depends=['c/_complexstuff.h'],
         include_dirs=[np.get_include(), ],
         extra_compile_args=['-fopenmp', '-O3', ],
