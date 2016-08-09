c0326e5911febd1b71905a6cd5af1b269ed60ad4
Gregory R. Lee <gregory.lee@cchmc.org>
2016-08-08 21:32:33 -0400

    update reinstall.sh

--- reinstall.sh
+++ reinstall.sh
@@ -3,4 +3,4 @@ pip uninstall pyir.nufft -y
 export LDFLAGS="$LDFLAGS -L/Users/lee8rx/anaconda/lib -Wl,-rpath,/Users/lee8rx/anaconda/lib"
 pip install -e . -v
 
-install_name_tool -add_rpath /Users/lee8rx/anaconda/lib /Users/lee8rx/src/my_git/pyir/pyir.nufft/pyir/nufft/_extensions/_nufft_table.cpython-35m-darwin.so
+# install_name_tool -add_rpath /Users/lee8rx/anaconda/lib /Users/lee8rx/src/my_git/pyir/pyir.nufft/pyir/nufft/_extensions/_nufft_table.cpython-35m-darwin.so
